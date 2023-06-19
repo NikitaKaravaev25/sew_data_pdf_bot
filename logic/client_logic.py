0854255418f9e17bdf086ce6918b0b1beb11c53b
srkarnik <@NikitaKaravaev25>
2023-06-19 10:24:38 +0300

    backup_logic

--- logic/client_logic.py
+++ logic/client_logic.py
@@ -1,28 +1,31 @@
+import asyncio
 from typing import Optional
 
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver import Chrome
 from bs4 import BeautifulSoup
 
 
 async def get_link(serial_number: str) -> Optional[str]:
-    options = Options()
-    options.add_argument("--headless")
-    driver = Chrome(options=options)
-
-    url = f"https://www.seweurodrive.com/os/dud/?tab=productdata&country=us&language=en_us&search={serial_number}&doc_lang=en-DE"
-
     try:
+        options = Options()
+        options.add_argument("--headless")
+        driver = webdriver.Chrome(options=options)
+
+        url = f"https://www.seweurodrive.com/os/dud/?tab=productdata&country=us&language=en_us&search={serial_number}&doc_lang=en-DE"
+
         driver.get(url)
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#productdata-pdf-button")))
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        text = soup.select_one("#productdata-pdf-button")
-        return text.get('href')
-    except:
-        return None
+        await asyncio.sleep(5)
+        for _ in range(12):
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            text = soup.find(id="productdata-pdf-button")
+            if text:
+                return text.get('href')
+            else:
+                await asyncio.sleep(2)
+    except Exception as e:
+        print(e)
+        return f'Exception: {e}'
     finally:
         driver.quit()
