d2123c4f1df369680868b603899a2ebd839eeec2
Nikita Karavaev <nikitakaravaev@mail.ru>
2023-07-11 17:00:50 +0300

    11.07.2023_speedup_v1

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@ from aiogram import executor
 
 from create_bot import dp
 from db import db_control
+from logic.client_logic import prepear_webdriver
+
 
 async def on_startup(_):
     await db_control.db_connect()
+    await prepear_webdriver()
     print("The bot has been started successfully!")
 
 
--- logic/client_logic.py
+++ logic/client_logic.py
@@ -2,29 +2,39 @@ import asyncio
 from typing import Optional
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
+async def prepear_webdriver():
+    global driver, original_window
+    options = Options()
+    # options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    original_window =driver.window_handles[0]
+    return driver
 
 async def get_link(serial_number: str) -> Optional[str]:
     try:
-        options = Options()
-        options.add_argument("--headless")
-        driver = webdriver.Chrome(options=options)
+        driver.switch_to.new_window('tab')
 
         url = f"https://www.seweurodrive.com/os/dud/?tab=productdata&country=us&language=en_us&search={serial_number}&doc_lang=en-DE"
 
         driver.get(url)
-        await asyncio.sleep(5)
-        for _ in range(20):
+        await asyncio.sleep(3)
+        for _ in range(26):
             html = driver.page_source
             soup = BeautifulSoup(html, 'html.parser')
             text = soup.find(id="productdata-pdf-button")
             if text:
                 return text.get('href')
             else:
-                await asyncio.sleep(2)
+                await asyncio.sleep(1)
     except Exception as e:
         return f'Exception: {e}'
     finally:
-        driver.quit()
+        driver.close()
+        driver.switch_to.window(original_window)
+
+
+
